f2e99cdfb9d24438f40b36281cabe08e52347f79
Rick Mills <rick.sketchy@gmail.com>
2013-07-05 16:15:50 +0100

    replaced native SessionHandlerInterface with a custom one.

--- tests/Aura/Session/MockSessionHandler.php
+++ tests/Aura/Session/MockSessionHandler.php
@@ -1,39 +1,39 @@
 <?php
 namespace Aura\Session;
 
-use SessionHandlerInterface;
+//use SessionHandlerInterface;
 
 // a session handler that does nothing, for testing purposes only
-class MockSessionHandler implements SessionHandlerInterface
+class MockSessionHandler implements SessionInterface
 {
     public $data;
-    
+
     public function close()
     {
         return true;
     }
-    
+
     public function destroy($session_id)
     {
         $this->data = null;
         return true;
     }
-    
+
     public function gc($maxlifetime)
     {
         return true;
     }
-    
+
     public function open($save_path, $session_id)
     {
         return true;
     }
-    
+
     public function read($session_id)
     {
         return $this->data;
     }
-    
+
     public function write($session_id, $session_data)
     {
         $this->data = $session_data;
